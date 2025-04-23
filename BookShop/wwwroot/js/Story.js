22fdc93aabf23960ccc6f57334aea2a62e46a364
MikaeelYadegar <165888959+MikaeelYadegar@users.noreply.github.com>
2025-04-23 20:42:47 +0430

    Story View Completed

---
+++ BookShop/wwwroot/js/Story.js
@@ -0,0 +1,36 @@
+﻿let timeout;
+function showStory(element) {
+    clearTimeout(timeout);
+    const modal = document.getElementById("storyModal");
+    const modalImage = document.getElementById("modalImage");
+    const modalVideo = document.getElementById("modalVideo");
+    clearTimeout(timeout);
+    if (element.tagName === "IMG") {
+        modalImage.src = element.src;
+        modalImage.style.display = "block";
+        modalVideo.style.display = "none";
+    }
+    else if (element.tagName === "Video") {
+        modalVideo.src = element.src;
+        modalVideo.style.display = "block";
+        modalImage.style.display = "none";
+        modalVideo.play();
+    }
+    modal.style.display = "flex";
+    
+    timeout = setTimeout(() => {
+        hideStory();
+    },5000);
+}
+function hideStory() {
+    const modal = document.getElementById("storyModal");
+    const modalImage = document.getElementById("modalImage");
+    const modalVideo = document.getElementById("modalVideo");
+    modal.style.display = "none";
+    modalImage.style.display = "none";
+    modalVideo.style.display = "none";
+    modalImage.src = "";
+    modalVideo.pause();
+    modalVideo.src = "";
+    clearTimeout(timeout);
+}
